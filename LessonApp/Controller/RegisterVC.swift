db4fadd8911f9d53efaa1b067f7b210c562d5c45
Zizo <zizoadel26@gmail.com>
2020-01-13 14:00:41 -0800

    commit register screen

--- LessonApp/Controller/RegisterVC.swift
+++ LessonApp/Controller/RegisterVC.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class RegisterVC: UIViewController {
+    
+    // MARK: - Variables
+    var leftButtons: [UIButton] = []
+    var leftConfirmPassButton: UIButton!
+    var rememberToggle: Bool = false
 
+    // MARK: - Outlets
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet var textFields: [UITextField]!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var maleBtn: UIButton!
+    @IBOutlet weak var femaleBtn: UIButton!
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initialConfigration()        
     }
     
-
+    // MARK: - Actions
+    @IBAction func approveAction() {
+        
+    }
+    
+    @IBAction func register() {
+        
+    }
+    
 }
+
+
